9a312eaf0aad411fbf66b47b5e0aa3185d9f86ec
Fin <anoun@mailbox.org>
2026-08-21 16:51:59 +0300

    Layout fixes.

--- src/Pages/VideosPage.tsx
+++ src/Pages/VideosPage.tsx
@@ -1,21 +1,37 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react'
 import { Header } from '../components/Header';
 import './VideosPage.css'
 
 export function VideosPage() {
-    const displayVideos = async () => {
-        const stream = await axios.get('/api/stream/Videos/Berserk.mp4')
-        await stream.data;
+    interface FileNode {
+        name: string;
+        isDir: boolean;
+        children?: FileNode[];
     }
+
+    interface ListDirResponse {
+        reply: string;
+        directory: FileNode;
+    }
+    useEffect(
+        () => {
+            const displayVideos = async () => {
+                const response = await axios.get('/api/dir/Videos?dirOnly=false&recDepth=99');
+                const Tree: ListDirResponse = response.data;
+                console.log(Tree.directory);
+
+            }
+            displayVideos();
+        }, []
+    )
+    
 
     return(
         <>
             <title>Videos</title>
             <Header />
             <div className="videos-page">
-                <video controls width="480">
-                    <source src='/api/stream/Videos/Berserk.mp4' />
-                </video>
             </div>
         </>
     );
